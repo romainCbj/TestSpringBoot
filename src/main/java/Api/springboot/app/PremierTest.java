de091c226a858050156424039268e8f44ad28d0b
Romain Desruelle <romaindesruelle@MacBook-Pro-de-Romain.local>
2024-07-28 22:11:38 +0200

    test different return

--- src/main/java/Api/springboot/app/PremierTest.java
+++ src/main/java/Api/springboot/app/PremierTest.java
@@ -1,4 +1,11 @@
 package Api.springboot.app;
+import java.awt.PageAttributes.MediaType;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +14,24 @@ public class PremierTest {
 
 	@GetMapping("/welcome")
 	public String Welcome() {
-		return "test api";
+		return "test api 2";
 	}
+	
+	@GetMapping("/testApi")
+	public Map<String, String> TestJson(){
+		 HashMap<String, String> map = new HashMap<>();
+		    map.put("key", "value");
+		    map.put("foo", "bar");
+		    map.put("aa", "bb");
+		    return map;
+	}
+	
+	@GetMapping(path = "/testResponse")
+    public ResponseEntity<Object> sayHello()
+    {
+
+		HashMap<String, String> map = new HashMap<>();
+        map.put("aa", "bb");
+        return new ResponseEntity<Object>(map, HttpStatus.OK);
+    }
 }
